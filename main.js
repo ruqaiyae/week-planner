117e329e36dc3842216ef5cbb3b03aaa112a4d91
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-01-20 19:43:37 +0000

    Created the dialog modal

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-const $select = document.querySelector('.select');
+var $select = document.querySelector('.select');
 if (!$select) throw new Error('$select query failed!');
-
-for (let i = 0; i < 25; i++) {
-  const $option = document.createElement('option');
+for (var i = 0; i < 25; i++) {
+  var $option = document.createElement('option');
   $option.setAttribute('value', String(i));
   $option.textContent = String(i);
-
   $select.appendChild($option);
 }
-
-const $newEvent = document.querySelector('.new-event');
+var $newEvent = document.querySelector('.new-event');
 if (!$newEvent) throw new Error('$newEvent query failed');
-
-const $dialog = document.querySelector('dialog');
+var $dialog = document.querySelector('dialog');
 if (!$dialog) throw new Error('$dialog query failed');
-
-$newEvent.addEventListener('click', () => {
+$newEvent.addEventListener('click', function () {
   $dialog.showModal();
 });
