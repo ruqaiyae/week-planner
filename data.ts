2b914fe92cb582e12b7d51de496fe0cc0eef2b97
Carolyne Golias <goliascarolyne@gmail.com>
2025-01-20 22:34:20 +0000

    updated

--- data.ts
+++ data.ts
@@ -3,3 +3,19 @@
 //   editing: Entry | null;
 //   status: boolean;
 // }
+
+// // const data = readData();
+
+// // function writeData(): undefined {
+//   // const dataJSON = JSON.stringify(data);
+//  // localStorage.setItem('data-storage', dataJSON);
+// // }
+
+// // function readData(): Data[] {
+//   // if (localStorage.getItem('data-storage')) {
+//     // const parsedJSON = JSON.parse(localStorage.getItem('data-storage') || '[]');
+//      // return parsedJSON;
+//   // } else {
+//  //    return [];
+// //  }
+// //  }
--- main.ts
+++ main.ts
@@ -1,40 +1,49 @@
+// // interface FormElements extends HTMLFormControlsCollection {
+// //   time: HTMLSelectElement;
+// //   day: HTMLSelectElement;
+// //   eventInfo: HTMLTextAreaElement;
+// // }
+
 // interface Entry {
 //   time: string;
 //   day: string;
 //   eventInfo: string;
 // }
 
-const $select = document.querySelector('.select');
-if (!$select) throw new Error('$select query failed!');
+// const $select = document.querySelector('.select');
+// if (!$select) throw new Error('$select query failed!');
 
-for (let i = 0; i < 25; i++) {
-  const $option = document.createElement('option');
-  $option.setAttribute('value', String(i));
-  $option.textContent = String(i);
+// for (let i = 0; i < 25; i++) {
+//   const $option = document.createElement('option');
+//   $option.setAttribute('value', String(i));
+//   $option.textContent = String(i);
 
-  $select.appendChild($option);
-}
+//   $select.appendChild($option);
+// }
 
-const $newEvent = document.querySelector('.new-event');
-if (!$newEvent) throw new Error('$newEvent query failed');
+// const $newEvent = document.querySelector('.new-event');
+// if (!$newEvent) throw new Error('$newEvent query failed');
 
-const $dialog = document.querySelector('dialog');
-if (!$dialog) throw new Error('$dialog query failed');
+// const $dialog = document.querySelector('dialog');
+// if (!$dialog) throw new Error('$dialog query failed');
 
-$newEvent.addEventListener('click', () => {
-  $dialog.showModal();
-});
+// $newEvent.addEventListener('click', () => {
+//   $dialog.showModal();
+// });
 
-const $cancelBtn = document.querySelector('.cancel-btn');
-const $confirmBtn = document.querySelector('.confirm-btn');
+// const $cancelBtn = document.querySelector('.cancel-btn');
+// const $entryForm = document.querySelector('.entry-form');
 
-if (!$cancelBtn) throw new Error('$cancelBtn query failed.');
-if (!$confirmBtn) throw new Error('$confirmBtn query failed.');
+// if (!$cancelBtn) throw new Error('$cancelBtn query failed.');
+// if (!$entryForm) throw new Error('$entryForm query failed.');
 
-$cancelBtn.addEventListener('click', () => {
-  $dialog.close();
-});
+// const $timeInput = document.querySelector('#time');
+// if (!$timeInput) throw new Error('timeInput query failed');
 
-// $confirmBtn.addEventListener('click' () => {
+// $cancelBtn.addEventListener('click', () => {
+//   $dialog.close();
+// });
 
-// })
+// $entryForm.addEventListener('submit', (event: Event) => {
+//   event.preventDefault();
+// });
